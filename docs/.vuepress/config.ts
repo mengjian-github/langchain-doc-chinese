da40b4d207301270ba4453a94aeb030b308a519a
erasermeng <erasermeng@tencent.com>
2023-04-06 18:40:25 +0800

    sidebar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,5 +2,20 @@ import { defineConfig } from "vuepress/config";
 
 export default defineConfig({
   title: "LangChain中文文档",
-  base: '/langchain-doc-chinese/'
+  base: "/langchain-doc-chinese/",
+  themeConfig: {
+    nav: [
+      {
+        text: "Github",
+        link: "https://github.com/mengjian-github/langchain-doc-chinese",
+      },
+    ],
+    sidebar: [
+      "/",
+      {
+        title: "入门",
+        children: ["/getting-started/install"],
+      },
+    ],
+  },
 });

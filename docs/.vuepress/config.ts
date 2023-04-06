aa733d5f4d78857dc58b06e6da68b809138e5209
erasermeng <erasermeng@tencent.com>
2023-04-06 16:34:42 +0800

    first commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vuepress/config";
+
+export default defineConfig({
+  title: "Hello VuePress",
+  description: "Just playing around",
+  base: '/langchain-doc-chinese/'
+});
